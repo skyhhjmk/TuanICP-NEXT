cb948ab07c3c3c2706d4adfb94d5e0758d7d3a37
hhjmk <admin@biliwind.com>
2025-02-05 13:16:46 +0800

    update

---
+++ data/plugins/help_me/main.php
@@ -0,0 +1,19 @@
+<?php
+/*
+* Name:        应用商店
+* Description:        向后台添加一个应用商店，支持在线下载主题和插件
+* Version:            1.0
+* Author:             风屿Wind
+*/
+
+define('APP_STORE_DIR', __DIR__);
+add_menu('admin_sidebar', '应用商店', get_Url('admin/app_store'));
+
+function appstore_add_page_router($page_router)
+{
+    $page_router['admin']['document'] = APP_STORE_DIR . '/pages/document.php';
+
+    return $page_router;
+}
+add_filter('page_router', 'appstore_add_page_router');
+
--- super/system_a/pages/id.php
+++ super/system_a/pages/id.php
@@ -2,18 +2,25 @@
 if (!defined('APP_ROOT')) {
     exit('Direct access is not allowed.');
 }
-// 获取URL参数 keyword
-$keyword = $_GET['keyword'] ?? '';
-if (!$keyword) {
+
+if (empty($_GET['keyword'])) {
     header('Location: /');
     exit();
 }
+
+// 获取URL参数 keyword
+$keyword = $_GET['keyword'] ?? '';
+$urlPattern = $_GET['keyword'] ?? '';
+
 $dbc = initDatabase();
 // 查询备案信息
 // 使用 OR 逻辑来查询备案号或域名
-$sql = "SELECT site_id, user_id, site_name, site_domain, site_icp_number, site_desc, site_avatar_url, site_config, site_status, site_ext, status, created_at FROM sites WHERE site_icp_number = :keyword OR site_domain LIKE :urlPattern";
+$sql = "SELECT site_id as site_id,site_name as site_name, site_domain as site_domain, 
+       site_icp_number as site_icp_number,
+       site_desc as site_desc, site_avatar_url as site_avatar_url, site_config as site_config,
+       site_status as site_status, site_ext as site_ext, status as status, created_at as created_at
+FROM sites WHERE site_icp_number = :keyword OR site_domain = :urlPattern";
 $stmt = $dbc->prepare($sql);
-$urlPattern = "%{$keyword}%"; // 用于模糊匹配URL
 $stmt->execute(['keyword' => $keyword, 'urlPattern' => $urlPattern]);
 $icp_record = $stmt->fetch(PDO::FETCH_ASSOC);
 
@@ -24,18 +31,52 @@ if (!$icp_record) {
     exit;
 }
 
-// 检查备案状态是否为“审核通过”
-if ($icp_record['STATUS'] !== '审核通过') {
-    // 如果状态不是“审核通过”，则弹窗提示用户
-    echo "<script type='text/javascript'>";
-    echo "alert('该备案信息未通过审核');";
-    echo "window.location.href = '/xg?keyword=" . urlencode($keyword) . "';";
-    echo "</script>";
-    exit; // 终止脚本执行
+global $icp_common_icp_record;
+
+switch ($icp_record['status']) {
+    case 'awaiting':
+        $status_msg = "该备案信息正在审核中，请正确悬挂信息并且耐心等待。";
+
+        $icp_common_icp_record = [
+            'site_domain' => '******',
+            'site_name' => '******',
+            'site_desc' => '******',
+            'site_avatar_url' => '******',
+            'site_icp_number' => $icp_record['site_icp_number'],
+            'created_at' => $icp_record['created_at'],
+        ];
+        break;
+    case 'approved':
+        $status_msg = "该备案信息已经通过审核，请正确悬挂信息。";
+        $icp_common_icp_record = $icp_record;
+        break;
+    case 'rejected':
+        $icp_common_icp_record = [
+            'site_domain' => '******',
+            'site_name' => '******',
+            'site_desc' => '******',
+            'site_avatar_url' => '******',
+            'site_icp_number' => $icp_record['site_icp_number'],
+            'created_at' => $icp_record['created_at'],
+        ];
+        $status_msg = "该备案信息已被驳回，请修正问题后重新提交审核。";
+        break;
+    default:
+        $icp_common_icp_record = [
+            'site_domain' => '******',
+            'site_name' => '******',
+            'site_desc' => '******',
+            'site_avatar_url' => '******',
+            'site_icp_number' => $icp_record['site_icp_number'],
+            'created_at' => $icp_record['created_at'],
+        ];
+        $status_msg = "未知状态";
+        break;
 }
 
-$icp_common_icp_record = $icp_record;
-global $icp_common_icp_record;
+$icp_common_icp_record['status_msg'] = $status_msg;
+//var_dump($icp_common_icp_record);
+
 function id_add_page_vars($page_vars)
 {
     global $icp_common_icp_record;
@@ -45,27 +86,16 @@ function id_add_page_vars($page_vars)
             'site_name' => $icp_common_icp_record['site_name'],
             'site_desc' => $icp_common_icp_record['site_desc'],
             'site_avatar_url' => $icp_common_icp_record['site_avatar_url'],
-            'site_config' => $icp_common_icp_record['site_config'],
-            'site_status' => $icp_common_icp_record['site_status'],
-            'site_ext' => $icp_common_icp_record['site_ext'],
-            'site_id' => $icp_common_icp_record['site_id'],
             'site_icp_number' => $icp_common_icp_record['site_icp_number'],
             'created_at' => $icp_common_icp_record['created_at'],
+            'site_status' => $icp_common_icp_record['status_msg'],
         ],
-        'user_icp_number' => $icp_common_icp_record['site_icp_number'],
-        'user_site_domain' => $icp_common_icp_record['site_domain'],
-        'user_website_name' => $icp_common_icp_record['website_name'],
-        'owner' => $icp_common_icp_record['owner'],
-        'status' => $icp_common_icp_record['STATUS'],
-        'user_site_desc' => $icp_common_icp_record['site_desc'],
-        'update_time' => $icp_common_icp_record['update_time'],
     ];
-
     $page_vars = array_merge($page_vars, $addVars);
 
     return $page_vars;
 }
-add_filter('page_router', 'icp_user_page_router');
+add_filter('page_vars', 'id_add_page_vars');
 
 $twig = initTwig();
 echo $twig->render('@index/id.html.twig', get_Page_vars());
