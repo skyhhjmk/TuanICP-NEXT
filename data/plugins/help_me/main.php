20b990855eecb406bd3563d19a3b6196569578e1
hhjmk <admin@biliwind.com>
2025-02-06 00:38:42 +0800

    update

--- data/plugins/help_me/main.php
+++ data/plugins/help_me/main.php
@@ -1,19 +1,19 @@
 <?php
 /*
-* Name:        应用商店
-* Description:        向后台添加一个应用商店，支持在线下载主题和插件
+* Name:        帮助文档
+* Description:        向后台添加一个帮助文档，提供系统的使用说明。
 * Version:            1.0
 * Author:             风屿Wind
 */
 
-define('APP_STORE_DIR', __DIR__);
-add_menu('admin_sidebar', '应用商店', get_Url('admin/app_store'));
+define('DOCUMENT_DIR', __DIR__);
+add_menu('admin_sidebar', '帮助文档', get_Url('admin/document'));
 
-function appstore_add_page_router($page_router)
+function help_me_document_add_page_router($page_router)
 {
-    $page_router['admin']['document'] = APP_STORE_DIR . '/pages/document.php';
+    $page_router['admin']['document'] = DOCUMENT_DIR . '/pages/document.php';
 
     return $page_router;
 }
-add_filter('page_router', 'appstore_add_page_router');
+add_filter('page_router', 'help_me_document_add_page_router');
 
